91ff99ed2f226a096e743343673b34abaa2325e3
Ryan Laurie <iethree@gmail.com>
2024-04-18 13:42:06 -0600

    remove generateKey prop

--- frontend/src/metabase/common/components/QuestionPicker/components/QuestionPicker.tsx
+++ frontend/src/metabase/common/components/QuestionPicker/components/QuestionPicker.tsx
@@ -23,12 +23,7 @@ import {
   type PickerState,
 } from "../../EntityPicker";
 import type { QuestionPickerOptions, QuestionPickerItem } from "../types";
-import {
-  generateKey,
-  getCollectionIdPath,
-  getStateFromIdPath,
-  isFolder,
-} from "../utils";
+import { getCollectionIdPath, getStateFromIdPath, isFolder } from "../utils";
 
 export const defaultOptions: QuestionPickerOptions = {
   showPersonalCollections: true,
@@ -172,7 +167,6 @@ export const QuestionPicker = ({
     <NestedItemPicker
       isFolder={(item: QuestionPickerItem) => isFolder(item, models)}
       options={options}
-      generateKey={generateKey}
       onFolderSelect={onFolderSelect}
       onItemSelect={handleItemSelect}
       path={path}
